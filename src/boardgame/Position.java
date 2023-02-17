56f97109c0bd777b35f203f217affd7ef1ca5ebe
Kauan Meira <kauangabriel.meira123@gmail.com>
2023-02-17 08:25:34 -0300

    King possible moves

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,8 +1,14 @@
 package boardgame;
 
 public class Position {
+
     private int row;
     private int column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
 
     public int getRow() {
         return row;
@@ -20,7 +26,7 @@ public class Position {
         this.column = column;
     }
 
-    public Position(int row, int column) {
+    public void setValues(int row, int column) {
         this.row = row;
         this.column = column;
     }
@@ -28,9 +34,5 @@ public class Position {
     @Override
     public String toString() {
         return row + ", " + column;
-    }
-    public void setValues(int row, int column){
-        this.row = row;
-        this.column = column;
     }
 }
